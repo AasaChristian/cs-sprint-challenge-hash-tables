0bdf0ba231a9b78a38f07e31a66779a8b09ba2f2
Aasa-Christian <aasachristian@yahoo.com>
2020-06-08 13:39:13 -0400

    completed first 3 ex's

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
+    loop thrue weights and add weight a
     """
-    # Your code here
+    hash = {}
+    count = 0
 
+    for item in weights:
+        needed = limit - item
+        if item in hash:
+            if count> hash[item]:
+                return count, hash[item]
+            else:
+                return hash[item], count
+        else:
+            hash[needed] = (count)
+
+        count += 1
+            
     return None
+w = [ 4, 6, 10, 15, 16 ] 
+
+print(get_indices_of_item_weights(w, 5, 21 ))
